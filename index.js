0a05aef10e35d6980116c82da09b90e7a9ba1de4
Andrew Southwick <Andrew.Southwick@us.usana.com>
2019-09-24 21:03:08 -0600

    using mongoDb client

--- index.js
+++ index.js
@@ -2,34 +2,81 @@ const express = require('express')
 const app  = express()
 const port = 3001
 const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://admin:passwordone@cluster0-khw7n.mongodb.net/test?retryWrites=true&w=majority";
+const instance = new MongoClient(uri, { useNewUrlParser: true,  useUnifiedTopology: true});
 
 app.use(bodyParser.json())
 app.use((req, res, next) => {
-     // Website you wish to allow to connect
      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-     // Request methods you wish to allow
      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
- 
-     // Request headers you wish to allow
      res.setHeader('Access-Control-Allow-Headers', 'application/json, content-type');
- 
-     // Set to true if you need the website to include cookies in the requests sent
-     // to the API (e.g. in case you use sessions)
      res.setHeader('Access-Control-Allow-Credentials', true);
- 
-     // Pass to next layer of middleware
      next();
 })
 
-let tvShowsArray = [] 
 
-app.post('/', (req, res) => {
-   tvShowsArray = [...tvShowsArray, req.body]
-   console.log(req.body)
-   res.send(tvShowsArray)
+const createInstance = (method, data) => {
+  return instance.connect((err, client) => {
+      if(err) err
+      const collection = client.db("tvdemo").collection("tvdemo")
+   switch(method){
+   case 'insertOne': 
+       return collection.insertOne(data)
+       break
+      case 'find':
+       console.log(collection.find().toArray())
+       break
+      default: 
+      return
+   }
 })
-app.get('/', (req, res) => res.send(tvShowsArray))
+}
+
+
+
+
+app.post('/tvDemo', (req, res)=> {
+console.log(createInstance('insertOne', req.body))
+res.send('success')
+
+   // instance.connect((err) => {
+   //    if(err) res.send(err)
+   //    const collection = client.db("tvdemo").collection("tvdemo")
+   //    collection.insertOne(req.body).then(r => res.send(r.ops))
+   // })
+
+
+   // instance.connect((err, client) => {
+   //    if(err) res.send(err)
+   //    const collection = client.db("tvdemo").collection("tvdemo")
+   //    collection.insertOne(req.body).then(r => res.send(r.ops))
+   //    db.close()
+   // })
+})
+   app.get('/getAllTvShows', async (req, res) => {
+      try{
+         let find = await createInstance('find')
+         res.send(find)
+        }catch(e){
+      console.log(e)
+      }
+     
+
+      // instance.connect((err, client)  => {
+      //    if(err) res.send(err)
+      //  const collection = client.db("tvdemo").collection("tvdemo")
+      //  collection.find().toArray().then(r => res.send(r))
+      // //  .toArray().then(r => res.send(r))
+      // //  db.close()
+      // }) 
+})
+app.get('/findShowWithId/:id', (req, res) => {
+   collection.findOne({_id: req.params.id}).then((data) => {
+      res.send(data)
+      db.close()
+   })
+})
 
 
 
